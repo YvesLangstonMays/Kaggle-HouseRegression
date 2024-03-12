2ea380155664647259219194529db1e967972e19
YvesLangstonMays <yveslangstonmays@gmail.com>
2024-03-12 15:41:03 -0500

    Initial

--- R/main.R
+++ R/main.R
@@ -1,9 +1,2 @@
-data <- read.csv("./house-prices-advanced-regression-techniques/train.csv")
-attach(data)
-
-
-data$Street <- NULL
-data <- na.omit(data)
-
-house.lm <- lm(SalePrice ~ BldgType, data = data)
-summary(house.lm)
+train <- read.csv("./house-prices-advanced-regression-techniques/train.csv")
+test <- read.csv("./house-prices-advanced-regression-techniques/test.csv")
